755949f575034a02b75688da282bff8285a5febf
Tarek <tareksaidee@hotmail.com>
2016-12-28 14:01:17 -0500

    Add parse characters JSON response

--- app/src/main/java/com/tareksaidee/marvel101/CharactersActivity.java
+++ app/src/main/java/com/tareksaidee/marvel101/CharactersActivity.java
@@ -1,7 +1,7 @@
 package com.tareksaidee.marvel101;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -21,5 +21,30 @@ public class CharactersActivity extends AppCompatActivity {
 
 
 
+    public static ArrayList<Character> extractCharacters(String JSONResponse) {
 
+        ArrayList<Character> characters = new ArrayList<>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(JSONResponse);
+            JSONArray results = jsonObject.getJSONArray("results");
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject curr = results.getJSONObject(i);
+                int id = curr.getInt("id");
+                String name = curr.getString("name");
+                String descrp = curr.getString("description");
+                JSONObject image = curr.getJSONObject("thumbnail");
+                String imageUrl = image.getString("path");
+                String imageExt = image.getString("extension");
+                Character character = new Character(name,id,descrp,imageUrl,imageExt);
+                characters.add(character);
+            }
+        } catch (JSONException e) {
+
+            Log.e("character JSON", "Problem parsing the character JSON results", e);
+        }
+
+        // Return the list of earthquakes
+        return characters;
+    }
 }
