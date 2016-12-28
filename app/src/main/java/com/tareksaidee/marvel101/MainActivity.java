f389c88cea993c79fa1da4902faa4977d3935fc7
Tarek <tareksaidee@hotmail.com>
2016-12-27 23:44:24 -0500

    Initial setup

--- app/src/main/java/com/tareksaidee/marvel101/MainActivity.java
+++ app/src/main/java/com/tareksaidee/marvel101/MainActivity.java
@@ -1,13 +1,26 @@
 package com.tareksaidee.marvel101;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button charactersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        charactersButton = (Button) findViewById(R.id.characters_button);
+        charactersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CharactersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
